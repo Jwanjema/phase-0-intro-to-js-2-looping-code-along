4745f1e4807eceefa5fd29af1eecbcf054140a26
joewaweruwanjema <wawerujoe@gmail.com>
2025-05-30 13:25:38 +0300

    add index.js test/indexTest.js package-lock.json

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,6 +2,21 @@ require ( './helpers.js' );
 
 const sinon = require( 'sinon' )
 
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
 describe( 'index.js', () => {
   let spy;
 
