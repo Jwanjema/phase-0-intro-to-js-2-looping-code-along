9a39c9a14bd99666a4ae762b952e3bfc7ad25eb1
joewaweruwanjema <wawerujoe@gmail.com>
2025-06-22 15:35:14 +0300

    add index.js test/indexTest.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-// Code your solutions in this file
-function writeCards(names, event) {
-  let messages = [];
+function writeCards(names) {
+  const thankYouMessages = []; // Create an empty array to store messages
   for (let i = 0; i < names.length; i++) {
-    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    // Push each formatted message into the array
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
   }
-  return messages;
+  return thankYouMessages; // Return the array of messages
 }
 
-function countDown(number) {
-  while (number >= 0) {
-    console.log(number);
-    number--;
+/**
+ * Logs numbers from the given number down to 0 (inclusive) to the console.
+ *
+ * @param {number} num - The starting number to count down from.
+ */
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
   }
 }
-
-module.exports = { writeCards, countDown };
