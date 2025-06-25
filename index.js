fdadd62d00e6b1637dc019c14fe2eb3c3ac7860b
joewaweruwanjema <wawerujoe@gmail.com>
2025-06-25 15:21:39 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-function writeCards(names) {
-  const thankYouMessages = []; // Create an empty array to store messages
-  for (let i = 0; i < names.length; i++) {
-    // Push each formatted message into the array
-    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+function writeCards (names, event) {
+  for ( let i = 0; i < names.length; i++ ) {
+    names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
   }
-  return thankYouMessages; // Return the array of messages
+  return names;
 }
 
-/**
- * Logs numbers from the given number down to 0 (inclusive) to the console.
- *
- * @param {number} num - The starting number to count down from.
- */
-function countDown(num) {
+function countDown (num) {
   while (num >= 0) {
     console.log(num);
     num--;
